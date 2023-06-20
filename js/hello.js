c8403d383f76bc16fc1505233ece6a7b66fea883
Muhammad Ali <48137657+alitheDev@users.noreply.github.com>
2023-06-20 08:15:56 +0100

    Create hello.js

---
+++ js/hello.js
@@ -0,0 +1,23 @@
+// Mock data
+const ordersData = {
+  totalOrders: 1500,
+  todaysOrders: 25
+};
+
+const salesData = {
+  totalSales: 50000,
+  todaysSales: 1500
+};
+
+const inventoryData = {
+  totalItems: 1000,
+  inStock: 500
+};
+
+// Update dashboard values
+document.getElementById('total-orders').textContent = ordersData.totalOrders;
+document.getElementById('todays-orders').textContent = ordersData.todaysOrders;
+document.getElementById('total-sales').textContent = salesData.totalSales;
+document.getElementById('todays-sales').textContent = salesData.todaysSales;
+document.getElementById('total-items').textContent = inventoryData.totalItems;
+document.getElementById('in-stock').textContent = inventoryData.inStock;
